c7a8cfb5e79d90733294ab30fe1d452ea0d317ae
PratikKr10 <pratik010kumar@gmail.com>
2025-04-07 20:05:52 +0000

    made changes to run.sh

--- run-jenkins.sh
+++ run-jenkins.sh
@@ -37,8 +37,20 @@ kubectl apply -n $NAMESPACE -f k8s/promtail-daemonset.yaml
 kubectl apply -n $NAMESPACE -f k8s/promtail-serviceaccount-rbac.yaml
 kubectl apply -n $NAMESPACE -f k8s/secrets
 kubectl apply -n $NAMESPACE -f k8s/services
+
+echo "⚙️ Creating frontend-config configmap dynamically..."
+
+export MINIKUBE_IP=$(minikube ip)
+export FRONTEND_PORT=$(kubectl get svc -n uber frontend-service -o=jsonpath='{.spec.ports[0].nodePort}')
+export FRONTEND_URL="http://$MINIKUBE_IP:$FRONTEND_PORT"
+
+kubectl create configmap frontend-config \
+  --from-literal=FRONTEND_URL="$FRONTEND_URL" \
+  --from-literal=VITE_BASE_URL="http://localhost:5000" \
+  --from-literal=VITE_API_URL="http://localhost:5000" \
+  -n uber --dry-run=client -o yaml | kubectl apply -f -
+
 kubectl apply -n $NAMESPACE -f k8s/configs
 kubectl apply -n $NAMESPACE -f k8s/deployments
-
 
 echo "✅ Build and deployment completed."
